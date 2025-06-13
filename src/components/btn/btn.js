d17dcfd0b2abe60a15581b682462dfbc7d290790
AnthonyBerri <anthonyberri030@gmail.com>
2025-06-13 10:09:39 -0300

    redirect to home

--- src/components/btn/btn.js
+++ src/components/btn/btn.js
@@ -3,7 +3,7 @@ class BtnComponent extends HTMLElement {
     super();
     const shadow = this.attachShadow({ mode: 'open' });
     shadow.innerHTML =`
-        <link rel="stylesheet" href="btn.css">
+        <link rel="stylesheet" href="../components/btn/btn.css">
         <div class="btn-container">
             <button class="the-btn">
                 <a href="#" id="btn-link"></a>
