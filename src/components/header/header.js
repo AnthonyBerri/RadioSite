b204927f58e953fc1d29c9ce3126e2b90ba1a9ec
AnthonyBerri <anthonyberri030@gmail.com>
2025-06-13 10:31:31 -0300

    fixing paths

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,7 @@ class HeaderComponent extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML =`
-        <link rel="stylesheet" href="components/header/header.css">
+        <link rel="stylesheet" href="../../components/header/header.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <header>
             <button class="menu-btn">☰ </button> 
@@ -18,15 +18,15 @@ class HeaderComponent extends HTMLElement {
     
             <div class="sidebar">
                 <ul>               
-                    <li><a href="src/pages/home/index.html"><i class="bi bi-house"></i> Home</a></li>
-                    <li><a href="src/pages/home/index.html"><i class="bi bi-star"></i> Favoritos</a></li>
+                    <li><a href="../../pages/home/index.html"><i class="bi bi-house"></i> Home</a></li>
+                    <li><a href="#"><i class="bi bi-star"></i> Favoritos</a></li>
                     <li><a href="#"><i class="bi bi-question-circle"></i> Sobre o projeto</a></li>
 
                     <h1>Estilos</h1>
 
-                    <li><a href="src/pages/pop/pop.html">Pop</a></li>
-                    <li><a href="#">Rock</a></li>
-                    <li><a href="#">2000's</a></li>
+                    <li><a href="../../pages/pop/pop.html">Pop</a></li>
+                    <li><a href="../../pages/rock/rock.html">Rock</a></li>
+                    <li><a href="../../pages/2000s/2000s.html">2000's</a></li>
 
                     <h1>Artistas</h1>
 
